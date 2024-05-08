0fb740418d2019ec661eaeebff853edb721d040e
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-05-09 00:31:02 +0600

    Class01_php

---
+++ php/class01/index.php
@@ -0,0 +1,9 @@
+<?php
+//A-Z,a-z,0-9,_
+// $first_Name=[2,1,1];
+// var_dump($first_Name);
+$first_Name="Jhon ";
+$last_name="Doe";
+// echo strpos($first_Name,"o");
+// var_dump(trim($first_Name));
+echo $first_Name."and ".$last_name;
